1f4b6e610f8afbb187dab56f2bc6010c3113dada
maxfang168 <159708634+maxfang168@users.noreply.github.com>
2024-11-21 16:42:29 -0500

    Update main.cpp fix error

--- main.cpp
+++ main.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+//Function that returns random long double weight.
+long double randomWeight() {
+    return (static_cast<long double>(rand()) / RAND_MAX) - 0.5;
+}
+
+//Add descriptor comment here
+int randomizeWeights() {
+//Finish here
+}
+
+//Program variables:
+int board[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //Represents board state (0 = empty, 1 = Player, -1 = ANN) (Read from left to right, top to down (English style))
+int gameNumber=0; //Keeps track of game number
+
+//Weights
+double inputWeights[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //Input layer weights
+double layer1Weights[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //Hidden layer #1 weights
+double layer2Weights[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //Hidden layer #2 weights
+double layer3Weights[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //Hidden layer #3 weights
+double outputWeights[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //Output layer weights
+
+//Biases
+//Input layer has no biases.
+double layer1Biases[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //Hidden layer #1 biases
+double layer2Biases[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //Hidden layer #2 biases
+double layer3Biases[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //Hidden layer #3 biases
+double outputBiases[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //Output layer biases
+
 
 int main() {
+    srand(time(0)); //Initial seed set for random num generation
     std::cout << "Welcome to Max\'s tic-tac-toe ANN!";
-    
-    //Program variables:
-    int board[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}; //Represents board state (0 = empty, 1 = Player, -1 = ANN)
-    int gamenumber=0; //Keeps track of game number
-    
+
+    //Function declarations
+    randomizeWeights();
     return 0;
 }
