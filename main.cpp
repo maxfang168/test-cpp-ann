c8eed1676e077baf6c922caa6cfde682657d4731
maxfang168 <159708634+maxfang168@users.noreply.github.com>
2024-11-20 22:37:42 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@ int main() {
     std::cout << "Welcome to Max\'s tic-tac-toe ANN!";
     
     //Program variables:
-    int board[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-
+    int board[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}; //Represents board state (0 = empty, 1 = Player, -1 = ANN)
+    int gamenumber=0; //Keeps track of game number
+    
     return 0;
 }
